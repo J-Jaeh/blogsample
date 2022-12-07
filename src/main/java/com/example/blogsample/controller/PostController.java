bb18c083dfc1830487c1f5b964a69dcb1f2e1d82
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2022-12-07 23:32:48 +0900

    POST컨트롤러 등록

---
+++ src/main/java/com/example/blogsample/controller/PostController.java
@@ -0,0 +1,17 @@
+package com.example.blogsample.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+    @GetMapping("/")
+    public ModelAndView home() {
+        return new ModelAndView("index");
+    }    //ModelAndView라는 객체에 데이터도 넣어줄수 있고 templates의 html파일 반환할수있다.!
+
+
+}
