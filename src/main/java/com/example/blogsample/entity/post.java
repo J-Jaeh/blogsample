2c82239b2e046f9dce9f3c6a3bcfed3c4ada0659
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2022-12-07 22:23:54 +0900

    부트스트랩+Post클래스

---
+++ src/main/java/com/example/blogsample/entity/post.java
@@ -0,0 +1,24 @@
+package com.example.blogsample.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Memo extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @Column(nullable = false, length = 4)
+    private int password;
+}
